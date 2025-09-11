ca37c7b80de7ceeae9bd4276b1263efa2d64432e
nwest0 <nwest000@citymail.cuny.edu>
2025-09-11 14:26:51 -0400

    bug fix + test

--- src/settings.ts
+++ src/settings.ts
@@ -15,12 +15,20 @@ export const profile = {
     ],
     publications: [
       {
-        title: "Pub 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod ultricies enim eget tincidunt. In eu mi iaculis, pulvinar augue eu, consectetur sapien. Nullam in risus ut erat lobortis aliquet. In aliquet quam nibh, sed finibus elit suscipit a. Nullam finibus pellentesque ornare. Curabitur ac diam euismod, dapibus eros a, pellentesque enim. Nulla et scelerisque neque. Aenean in magna consectetur, sodales arcu vel, luctus nisl. Nulla facilisi. Mauris quis tempus enim. Donec a porta sapien, a consequat est. Proin egestas, ipsum eu eleifend pretium, arcu leo viverra elit, nec consequat nunc ex nec quam. Nulla in luctus elit, ac suscipit nibh. Quisque consequat metus quis lorem pellentesque, at imperdiet ante varius."
-    },
-    {
-        title: "Pub 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer euismod ultricies enim eget tincidunt. In eu mi iaculis, pulvinar augue eu, consectetur sapien. Nullam in risus ut erat lobortis aliquet. In aliquet quam nibh, sed finibus elit suscipit a. Nullam finibus pellentesque ornare. Curabitur ac diam euismod, dapibus eros a, pellentesque enim. Nulla et scelerisque neque. Aenean in magna consectetur, sodales arcu vel, luctus nisl. Nulla facilisi. Mauris quis tempus enim. Donec a porta sapien, a consequat est. Proin egestas, ipsum eu eleifend pretium, arcu leo viverra elit, nec consequat nunc ex nec quam. Nulla in luctus elit, ac suscipit nibh. Quisque consequat metus quis lorem pellentesque, at imperdiet ante varius."
+        title: "Area 1",
+        authors: "Author 1, Author 2, Author 3",
+        journal: "Journal Name",
+        time: "Year",
+        link: "Coming soon",
+        abstract: "Coming soon"
+      },
+      {
+        title: "Area 2",
+        authors: "Author 1, Author 2, Author 3",
+        journal: "Journal Name",
+        time: "Year",
+        link: "Coming soon",
+        abstract: "Coming soon"
       },
     ],
     lab_members: [
